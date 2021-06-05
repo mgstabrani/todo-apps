8bab062541e91611949fb4859f16e370287ad838
mgstabrani <mgstabrani19@gmail.com>
2021-06-05 10:28:03 +0700

    Add completed feature

--- js/dom.js
+++ js/dom.js
@@ -1,12 +1,11 @@
 const UNCOMPLETED_LIST_TODO_ID = "todos";
+const COMPLETED_LIST_TODO_ID = "completed-todos"; 
  
 function addTodo(){
     const uncompletedTODOList = document.getElementById(UNCOMPLETED_LIST_TODO_ID );
  
     const textTodo = document.getElementById("title").value;
     const timestamp = document.getElementById("date").value;
-    console.log("todo" + textTodo);
-    console.log("timestamp" + timestamp);
  
     const todo = makeTodo(textTodo, timestamp);
     uncompletedTODOList.append(todo);
@@ -27,6 +26,26 @@ function makeTodo(data, timestamp) {
     const container = document.createElement("div");
     container.classList.add("item", "shadow")
     container.append(textContainer);
+    container.append(createCheckButton());
  
     return container;
+}
+
+function createButton(buttonTypeClass , eventListener) {
+    const button = document.createElement("button");
+    button.classList.add(buttonTypeClass);
+    button.addEventListener("click", function (event) {
+        eventListener(event);
+    });
+    return button;
+}
+
+function addTaskToCompleted(taskElement) {
+    taskElement.remove();
+}
+
+function createCheckButton() {
+    return createButton("check-button", function(event){
+         addTaskToCompleted(event.target.parentElement);
+    });
 }
